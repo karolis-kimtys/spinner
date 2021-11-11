48467b5776b182fbcbeb940411f00018c5c7e978
Karolis Kimtys <karolis_k_7@hotmail.com>
2021-11-11 00:01:45 +0000

    intial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Spinner from './Spinner';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Spinner
+        loaderTitle="Loading....."
+        fontSize="50px"
+        position="center"
+        fontColor="blue"
+        spinnerWidth="50px"
+        spinnerHeight="50px"
+        fontFamily="inherit"
+      />
     </div>
   );
 }
-
-export default App;
